2db9cd0eaeef4f09a0bd83f0224ccca0e3dcc19a
Diversant91 <lex0422@mail.ru>
2022-10-03 01:44:57 +0700

    диапазон четных чисел

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <=a)
+{
+    if (count % 2 ==0)
+    {
+        Console.Write(count + ", ");
+    }
+    count++;
+
+}
+
