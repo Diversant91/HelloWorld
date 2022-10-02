89e5e848a43e547e9d3571d4e1e6983ee4c39478
Diversant91 <lex0422@mail.ru>
2022-10-03 00:25:01 +0700

    Наибольшее число

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a>b)
+{max = a;
+Console.WriteLine($"Max = {max}");
+}
+if (a<b)
+{max = b;
+Console.WriteLine($"Max = {max}");
+}
