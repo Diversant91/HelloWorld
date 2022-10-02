9c74b55580373c0a5db2967f9190d894b55df704
Diversant91 <lex0422@mail.ru>
2022-10-03 00:58:13 +0700

    3 числа

---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    if(a>c)
+    {
+        Console.WriteLine(a);
+    }
+    else 
+    {Console.WriteLine(c);}
+    
+}
+if (a<b);
+{
+    if(b>c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {Console.WriteLine(c);}
+}
