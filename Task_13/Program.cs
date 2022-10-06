c5f0b467d082b7b10649b3e1e82ebda632464aa8
Diversant91 <lex0422@mail.ru>
2022-10-06 23:42:29 +0700

    3e число, если 2 то нет

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a >= 100)
+{int b = a%10;
+Console.WriteLine(b);
+}
+else 
+{
+   Console.WriteLine("третьей цифры нет");
+}
