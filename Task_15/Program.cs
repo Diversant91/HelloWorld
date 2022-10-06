12b31607ed4e0411c3260c24377a26fe9d2d0e57
Diversant91 <lex0422@mail.ru>
2022-10-07 00:16:45 +0700

    дни недели-выходные

---
+++ Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введи число от 1 до 7");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a < 1) || (a > 7))
+{
+    Console. WriteLine("Не корректное число, введи от 1 до 7");
+}
+if ((a==1)||(a==2)||(a==3)||(a==4)||(a==5))
+{
+  Console. WriteLine("Нет");  
+} 
+if ((a==6) || (a==7))
+{
+  Console. WriteLine("Да");  
+} 
