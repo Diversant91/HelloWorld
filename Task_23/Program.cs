c968c527224004feed04c91b8eb84a18ed7d3d53
Diversant91 <lex0422@mail.ru>
2022-10-08 01:28:08 +0700

    Куб

---
+++ Task_23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <=a)
+{
+    double cube = Math.Pow(count,3);
+    Console.Write(cube + ", ");
+
+    count++;
+}
